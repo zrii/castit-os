533fd7402079a3b49bd679a587361f06f7a7ea7a
zoranFFWD <zoran@futureforward.rs>
2026-01-22 10:59:37 +0100

    testig installable flow

--- flake.nix
+++ flake.nix
@@ -3,23 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; 
-    nixos-hardware.url = "github:nixos/nixos-hardware"; 
+    nixos-hardware.url = "github:nixos/nixos-hardware";
   };
 
   outputs = { self, nixpkgs, nixos-hardware, ... }: {
     nixosConfigurations = {
       
-      # --- INTEL NUC PLAYER ---
+      # 1. THE ACTUAL PLAYER (Target for the SSD)
+      # This is what you install ONTO the NUC's internal drive.
       intel-player = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          # Import the hardware config generated during installation
+          ./hardware-configuration.nix 
           ({ pkgs, ... }: {
-            # REQUIRED FOR INTEL NUC BOOT
-            boot.loader.grub.enable = false;
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
-            services.xserver.videoDrivers = [ "modesetting" ];
+            boot.loader.systemd-boot.enable = true; [cite: 34]
+            boot.loader.efi.canTouchEfiVariables = true; [cite: 34]
+            services.xserver.videoDrivers = [ "modesetting" ]; [cite: 34, 35]
+          })
+        ];
+      };
+
+      # 2. THE GRAPHICAL INSTALLER (The Flash Drive)
+      # This builds the ISO you boot from in WSL.
+      installer = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Provides the GNOME graphical installer environment
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix" [cite: 32]
+          
+          # This ensures your signage config files are available on the Live USB
+          ({ pkgs, ... }: {
+            environment.systemPackages = [ pkgs.git pkgs.vim ];
+            # Optional: Pre-configure networking or SSH for the installer if needed
           })
         ];
       };
@@ -28,16 +45,14 @@
       rpi-player = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
-          nixos-hardware.nixosModules.raspberry-pi-4
-          ./configuration.nix
+          nixos-hardware.nixosModules.raspberry-pi-4 [cite: 37]
+          ./configuration.nix [cite: 37]
           ({ pkgs, ... }: {
-            # RPi Specifics
-            boot.loader.grub.enable = false;
-            boot.loader.generic-extlinux-compatible.enable = true;
+            boot.loader.grub.enable = false; [cite: 37]
+            boot.loader.generic-extlinux-compatible.enable = true; [cite: 37]
           })
         ];
       };
-
     };
   };
 }
