ddaf3bd53097f5700337b47f09872987ea78b8f8
zoran <zoran@futureforward.rs>
2026-01-22 11:48:23 +0100

    Refactor comments in flake.nix for clarity and consistency

--- flake.nix
+++ flake.nix
@@ -9,14 +9,14 @@
   outputs = { self, nixpkgs, nixos-hardware, ... }: {
     nixosConfigurations = {
       
-      # 1. THE ACTUAL PLAYER (Target for the SSD)
+      # --- INTEL NUC PLAYER (Permanent SSD Install) ---
       intel-player = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          # This will be created on the NUC later
-          ./hardware-configuration.nix 
+          # Note: hardware-configuration.nix is required for the final install
           ({ pkgs, ... }: {
+            boot.loader.grub.enable = false;
             boot.loader.systemd-boot.enable = true;
             boot.loader.efi.canTouchEfiVariables = true;
             services.xserver.videoDrivers = [ "modesetting" ];
@@ -24,19 +24,19 @@
         ];
       };
 
-      # 2. THE GRAPHICAL INSTALLER (The Flash Drive)
+      # --- GRAPHICAL INSTALLER (The Flash Drive) ---
       installer = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          # Standard graphical installer module
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
           ({ pkgs, ... }: {
+            # Extra tools for the live environment
             environment.systemPackages = [ pkgs.git pkgs.vim ];
           })
         ];
       };
 
-      # 3. RASPBERRY PI PLAYER
+      # --- RASPBERRY PI PLAYER ---
       rpi-player = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
